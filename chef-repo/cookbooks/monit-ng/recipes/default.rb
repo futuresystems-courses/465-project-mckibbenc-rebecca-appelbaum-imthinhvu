5a3f1a9211978d4d5b0900121f04dad224560df3
Thinh Vu <im.thinhvu@gmail.com>
2015-05-25 22:02:37 -0400

    Updated with working Ansible script for Spark install.

--- chef-repo/cookbooks/monit-ng/recipes/default.rb
+++
@@ -1,7 +0,0 @@
-#
-# Cookbook Name:: monit-ng
-# Recipe:: default
-#
-
-include_recipe "#{cookbook_name}::#{node['monit']['install_method']}"
-include_recipe "#{cookbook_name}::config" if node['monit']['configure']
