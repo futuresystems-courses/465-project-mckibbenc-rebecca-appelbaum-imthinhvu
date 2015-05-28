ebc0e8b546a8c26875e669350bdcbca0b2006ea4
Colin McKibben <mckibbenc@gmail.com>
2015-05-28 03:14:44 +0000

    more command changes.  This almost works.  Just need to figure out ssh keys.

--- cloudmesh_spark/cloudmesh_spark/command_spark.py
+++ cloudmesh_spark/cloudmesh_spark/command_spark.py
@@ -18,22 +18,26 @@ class command_spark(object):
         subprocess.call("rm $CM_SPARK_DIR/node_keys/%s.pub"% (name), shell=True)
         subprocess.call("echo \"[spark-cluster]\" > $CM_SPARK_DIR/inventory/%s_inventory.txt"% (name), shell=True)
         subprocess.call("ssh-keygen -t rsa -f $CM_SPARK_DIR/node_keys/%s -P \"\""% (name), shell=True)
-        slaves = ""
+        slaves = "\n"
         ip_list = []
         for node in nodes:
             subprocess.call("cm vm ip assign %s"% (node), shell=True)
             time.sleep(2)
             output = subprocess.check_output("cm vm ip show %s --format=json"% (node), shell=True)
             ip = output.split("\n")[4].split(":")[1].split("\"")[1]
-            slaves = ip + "\n"
+            slaves += ip + "\n"
             ip_list.append(ip)
             subprocess.call("echo \"%s\" >> $CM_SPARK_DIR/inventory/%s_inventory.txt"% (ip, name), shell=True)
             subprocess.call("scp $CM_SPARK_DIR/node_keys/%s ubuntu@%s:~/.ssh/id_rsa"% (name, ip), shell=True)
             subprocess.call("scp $CM_SPARK_DIR/node_keys/%s.pub ubuntu@%s:~/.ssh/id_rsa.pub"% (name, ip), shell=True)
             subprocess.call("ssh-copy-id -i $CM_SPARK_DIR/node_keys/%s ubuntu@%s"% (name, ip), shell=True)
-        subprocess.call("ansible-playbook -i $CM_SPARK_DIR/inventory/%s_inventory.txt -c ssh $CM_SPARK_DIR/ansible/spark.yaml"% (name), shell=True)
         for ip_address in ip_list:
-            subprocess.call("ssh ubuntu@%s \'echo \"%s\" >> /home/ubuntu/spark-1.3.1-bin-hadoop2.6/conf/slaves.template\'"% (ip_address, slaves))
+            subprocess.call("ssh ubuntu@%s \'exit\'"% (ip_address), shell=True)
+
+        subprocess.call("ansible-playbook -i $CM_SPARK_DIR/inventory/%s_inventory.txt -c ssh $CM_SPARK_DIR/ansible/spark.yaml"% (name), shell=True)
+
+        for ip_address in ip_list:
+            subprocess.call("ssh ubuntu@%s \'echo \"%s\" >> /home/ubuntu/spark-1.3.1-bin-hadoop2.6/conf/slaves.template\'"% (ip_address, slaves), shell=True)
             
 
     @classmethod
