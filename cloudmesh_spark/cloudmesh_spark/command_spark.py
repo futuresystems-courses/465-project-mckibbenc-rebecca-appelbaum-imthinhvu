b9cb4d18c7084cd0ebc1b0f45871ac4d422f6b9d
Colin McKibben <mckibbenc@gmail.com>
2015-05-29 03:35:17 +0000

    updated spark start command so that it actually pushes the spark-env.txt file to all nodes

--- cloudmesh_spark/cloudmesh_spark/command_spark.py
+++ cloudmesh_spark/cloudmesh_spark/command_spark.py
@@ -35,19 +35,17 @@ class command_spark(object):
             for ip_address in ip_list:
                 #subprocess.call("ssh ubuntu@%s \'exit\'"% (ip_address), shell=True)
                 subprocess.call("scp $CM_SPARK_DIR/hosts/%s_hosts.txt ubuntu@%s:~/hosts.txt"% (name, ip_address), shell=True) 
-                subprocess.call("scp $CM_SPARK_DIR/spark-env/%s_spark-env.txt ubuntu@%s:~/spark-env.txt"% (name, ip_address), shell=True) 
-
+    
         subprocess.call("ansible-playbook -i $CM_SPARK_DIR/inventory/%s_inventory.txt -c ssh $CM_SPARK_DIR/ansible/spark.yaml"% (name), shell=True)
 
         for ip_address in ip_list:
-            #subprocess.call("ssh ubuntu@%s \'cp /home/ubuntu/spark-1.3.1-bin-hadoop2.6/conf/slaves.template /home/ubuntu/spark-1.3.1-bin-hadoop2.6/conf/slaves"% (ip_address), shell=True)
-            #subprocess.call("ssh ubuntu@%s \'cp /home/ubuntu/spark-1.3.1-bin-hadoop2.6/conf/spark-env.sh.template /home/ubuntu/spark-1.3.1-bin-hadoop2.6/conf/spark-env.sh"% (ip_address), shell=True)
+            subprocess.call("ssh ubuntu@%s \'cp /home/ubuntu/spark-1.3.1-bin-hadoop2.6/conf/slaves.template /home/ubuntu/spark-1.3.1-bin-hadoop2.6/conf/slaves"% (ip_address), shell=True)
             subprocess.call("ssh ubuntu@%s \'echo \"%s\" >> /home/ubuntu/spark-1.3.1-bin-hadoop2.6/conf/slaves\'"% (ip_address, slaves), shell=True)
             subprocess.call("scp $CM_SPARK_DIR/node_keys/%s ubuntu@%s:~/.ssh/id_rsa"% (name, ip_address), shell=True)
             subprocess.call("scp $CM_SPARK_DIR/node_keys/%s.pub ubuntu@%s:~/.ssh/id_rsa.pub"% (name, ip_address), shell=True)
             subprocess.call("ssh-copy-id -i $CM_SPARK_DIR/node_keys/%s ubuntu@%s"% (name, ip_address), shell=True)
 
-            
+        print("\n\n\n\n\n\n***************************\n", slaves)    
 
     @classmethod
     def destroy(cls, name):
@@ -57,11 +55,18 @@ class command_spark(object):
     @classmethod
     def start(cls, master):
         output = subprocess.check_output("cm vm ip show %s --format=json"% (master), shell=True)
-        ip = output.split("\n")[4].split(":")[1].split("\"")[1]
+        master_ip = output.split("\n")[4].split(":")[1].split("\"")[1]
+        name_pieces = master.split("_")
+        name = name_pieces[0] + "_" + name_pieces[1]
         subprocess.call("rm $CM_SPARK_DIR/spark-env/%s_spark-env.txt"% (name), shell=True)
-        subprocess.call("echo \"export SPARK_MASTER_IP=%s\nexport SPARK_MASTER_PORT=7077\nexport SPARK_WORKER_INSTANCES=2\" >> $CM_SPARK_DIR/spark-env/%s_spark-env.txt"% (ip, name), shell=True)
+        subprocess.call("echo \"export SPARK_MASTER_IP=%s\nexport SPARK_MASTER_PORT=7077\nexport SPARK_WORKER_INSTANCES=2\" >> $CM_SPARK_DIR/spark-env/%s_spark-env.txt"% (master_ip, name), shell=True)
+        ip_list = subprocess.check_output("cat $CM_SPARK_DIR/inventory/%s_inventory.txt"% (name), shell=True)
+        ip_list = ip_list[1:-1]
+        for ip in ip_list:
+            subprocess.call("scp $CM_SPARK_DIR/spark-env/%s_spark-env.txt ubuntu@%s:~/spark-env.txt"% (name, ip), shell=True) 
+
         subprocess.call("ansible-playbook -i $CM_SPARK_DIR/inventory/%s_inventory.txt -c ssh $CM_SPARK_DIR/ansible/spark-env.yaml"% (name), shell=True)
-        subprocess.call("ssh ubuntu@%s \'bash /home/ubuntu/spark-1.3.1-bin-hadoop2.6/sbin/start-all.sh\'"% (ip), shell=True)
+        subprocess.call("ssh ubuntu@%s \'bash /home/ubuntu/spark-1.3.1-bin-hadoop2.6/sbin/start-all.sh\'"% (master_ip), shell=True)
 
 
     @classmethod
